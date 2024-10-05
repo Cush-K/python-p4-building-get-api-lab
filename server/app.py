5996e8771b2b5cf95647562c618816ab5e0b9d2d
Kalvine <kkalvine4@gmail.com>
2024-10-06 02:56:51 +0300

    creates a GET API

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,29 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
-
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    return make_response(bakeries, 200)
+    
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_dict = bakery.to_dict()
+        
+    return make_response(bakery_dict, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    good_by_price = [good.to_dict() for good in BakedGood.query.order_by(desc(BakedGood.price)).all()]
+        
+    return make_response(good_by_price, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_exp = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    good_dict = most_exp.to_dict()
+    
+    return make_response(good_dict, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
